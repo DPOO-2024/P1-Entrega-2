0590c08460e2294e9063691efa02c777196a2838
AnaSanabria <98646074+AnaSanabria@users.noreply.github.com>
2024-04-18 01:24:19 -0500

    A

--- Entrega2/src/Modelo/Galeria.java
+++ Entrega2/src/Modelo/Galeria.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 import Exceptions.MesajedeErrorException;
 import Usuarios.*;
